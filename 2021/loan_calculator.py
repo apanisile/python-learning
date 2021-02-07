8aa957262d808ff1d7178b039b6c3ea7c068858d
Ifeoluwa <apanisileifeoluwa@gmail.com>
2021-02-07 02:56:21 +0100

    added if ststement to match and verify email

--- 2021/loan_calculator.py
+++ 2021/loan_calculator.py
@@ -1,7 +1,13 @@
-#Python program to calculate laon eligibility
+#Python program to calculate loan
 
 fname = input("Hi there! Please enter your first name: ")
 lname = input("Please enter your last name: ")
+email = input("Enter your e-mail address: ")
+email2 = input("Enter your e-mail address again: ")
+
+if email != email2:
+    print("The email's dont match")
+    exit(1)
 
 bank_credit = int(input("Please what is your bank credit score? "))
 
@@ -10,6 +16,7 @@ bench_mark = 10000
 if bank_credit >= bench_mark :
     print("""Great! you qualify for a $5000 loan! How would you want to collect your loan?""")
     loan = input("Great! How much would you like to loan?: ")
+    
     
 else:
     print(f"Sorry {lname} {fname}! You do not qualify for our bank loan")
