a655d4fdb548c9f7aa313c6ac2056e911d143de0
apanisile <apanisileifeoluwa@gmail.com>
2021-02-10 07:11:53 +0100

    called the functions

--- 2021/loan_calculator.py
+++ 2021/loan_calculator.py
@@ -1,15 +1,17 @@
 #Python program to calculate loan
 import user_details
+from user_details import *
         
-user_details.name()
-user_details.email()
+user = loan_function()
+user.name()
+user.email()
 
 bank_credit = int(input("Please what is your bank credit score? "))
 
 bench_mark = 10000
 
 if bank_credit >= bench_mark :
-    user_details.payment()
+    user.payment()
 else:
     print(f"Sorry! You do not qualify for our bank loan")
     
