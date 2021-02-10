669173a786c4866e49b634bb652f82a3c9bb7c84
apanisile <apanisileifeoluwa@gmail.com>
2021-02-10 12:27:53 +0100

    Added more options for the prompt to start

--- 2021/loan_calculator.py
+++ 2021/loan_calculator.py
@@ -1,17 +1,27 @@
 #Python program to calculate loan
-import user_details
+
 from user_details import *
-        
-user = loan_function()
-user.name()
-user.email()
 
-bank_credit = int(input("Please what is your bank credit score? "))
+prompt_user = input("""Do you want to sign in? (Y)es or (N)o 
+    > """)
 
-bench_mark = 10000
+if (prompt_user == "Y") | (prompt_user == "y") | (prompt_user == "yes") | (prompt_user == "Yes"):
 
-if bank_credit >= bench_mark :
-    user.payment()
+    user = loan_function()
+    user.name()
+    user.email()
+
+    bank_credit = int(input("Please what is your bank credit score? "))
+
+    bench_mark = 10000
+
+    while bench_mark > 0:
+        if bank_credit >= bench_mark:
+            user.payment()
+            print("Your account would be credited in the next 2-5 working days")
+        else:
+            print(f"Sorry! You do not qualify for our bank loan")
+
 else:
-    print(f"Sorry! You do not qualify for our bank loan")
-    
+    print("Bye!")
+    exit(0)
